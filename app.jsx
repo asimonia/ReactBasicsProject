0b2224d1ba3f4d3083c537de7f347aa24c5ee780
Alex Simonian <alex.simonian@gmail.com>
2017-03-27 17:10:15 -0500

    Create and render first React component

--- app.jsx
+++ app.jsx
@@ -1,1 +1,12 @@
-ReactDOM.render(<h1>Hello!</h1>, document.getElementById('container'));
+// React Component returing JSX
+function Application() {
+	return (
+		<div>
+			<h1>Hello from React!</h1>
+			<p>I was rendered from the Application component</p>
+		</div>
+	);
+}
+
+// Rendering the React component to the Virtual DOM
+ReactDOM.render(<Application />, document.getElementById('container'));
