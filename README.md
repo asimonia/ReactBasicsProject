61efd7a95cf2a3c7e01772fc9682c463dbcdbfb8
Alex Simonian <alex.simonian@gmail.com>
2017-03-28 14:34:54 -0500

    Restructuring State

--- README.md
+++ README.md
@@ -45,3 +45,20 @@ State is data in our application that can change.
 getInitialState: Component method for defining the initial state of your component.
 Flux: A pattern for organizing your state in an application.
 Redux: A popular library for managing application state and state changes.
+
+## Unidirectional Data Flow
+All data in our application flow in a single direction.  In React it flows
+down the tree from parent to child.  This makes tracking the source and
+destination easy compared to other archtectures where data may be coming
+may be coming from many parts of the application.
+
+## Application State
+The state or data in our application that is core to the functionality of the application
+as a whole.  This usually includes a list of the models and data being manipulated
+by the interface.  If we were to reload our application, the Application state is what
+we would like to persist the most.
+
+## Local Component State
+This is state that is used to allow a component to a function.  Local component state
+is typically not used by other components in the application, and is less
+important to persist if the application resets.
--- app.jsx
+++ app.jsx
@@ -1,123 +1,111 @@
 var PLAYERS = [
-	{
-		name: "Alex Simonian",
-		score: 42,
-		id: 1,
-	},
-	{
-		name: "Darth Vader",
-		score: 50,
-		id: 2,
-	},
-	{
-		name: "Bill Simmons",
-		score: 30,
-		id: 3,
-	},
-]
+  {
+    name: "Jim Hoskins",
+    score: 31,
+    id: 1,
+  },
+  {
+    name: "Andrew Chalkley",
+    score: 35,
+    id: 2,
+  },
+  {
+    name: "Alena Holligan",
+    score: 42,
+    id: 3,
+  },
+];
 
-// Header component has property, which must be a string
-Header.propTypes = {
-	title: React.PropTypes.string.isRequired,
-};
-
-// Header component returns jsx with a title
 function Header(props) {
-	return (
-		<div className="header">
-			<h1>{props.title}</h1>
-		</div>		
-	);
+  return (
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
+  );
 }
 
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired,
+};
+
 var Counter = React.createClass({
-	propTypes: {
-		initialScore: React.PropTypes.number.isRequired,
-	},
-
-	getInitialState: function() {
-		return {
-			score: this.props.initialScore,
-		}
-	},
-
-	incrementScore: function() {
-		this.setState({
-			score: (this.state.score + 1),
-		});
-	},
-
-	decrementScore: function() {
-		this.setState({
-			score: (this.state.score - 1),
-		});
-	},
-
-	render: function() {
-		return (
-			<div className="counter">
-				<button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-				<div className="counter-score"> {this.state.score} </div>
-				<button className="counter-action increment" onClick={this.incrementScore}> + </button>
-			</div>	
-		);
-	}
+  propTypes: {
+    initialScore: React.PropTypes.number.isRequired,
+  },
+  
+  getInitialState: function() {
+    return {
+      score: this.props.initialScore,
+    }
+  },
+  
+  incrementScore: function() {
+    this.setState({
+      score: (this.state.score + 1),
+    });
+  },
+  
+  decrementScore: function() {
+    this.setState({
+      score: (this.state.score - 1),
+    });
+  },
+  
+  render: function() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+        <div className="counter-score"> {this.state.score} </div>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+      </div>
+    );
+  }
 });
 
-// Counter component has property, which must be a number
-Counter.propTypes = {
-	score: React.PropTypes.number.isRequired,
-};
 
-
-// Player properties are name (string) and score (number)
-Player.propTypes = {
-	name: React.PropTypes.string.isRequired,
-	score: React.PropTypes.number.isRequired,
-};
-
-// Player component
 function Player(props) {
-	return (
-		<div className="player">
-			<div className="player-name">
-				{props.name}
-			</div>
-			<div className="player-score">
-				<Counter initialScore={props.score} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="player">
+      <div className="player-name">
+        {props.name}
+      </div>
+      <div className="player-score">
+        <Counter initialScore={props.score} />
+      </div>
+    </div>
+  );
+}
+
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired,
+};
+
+function Application(props) {
+  return (
+    <div className="scoreboard">
+      <Header title={props.title} />
+    
+      <div className="players">
+        {props.players.map(function(player) {
+          return <Player name={player.name} score={player.score} key={player.id} />
+        })}
+      </div>
+    </div>
+  );
 }
 
 Application.propTypes = {
-	title: React.PropTypes.string,
-	players: React.PropTypes.arrayOf(React.PropTypes.shape({
-		name: React.PropTypes.string.isRequired,
-		score: React.PropTypes.number.isRequired,
-	})).isRequired,
+  title: React.PropTypes.string,
+  players: React.PropTypes.arrayOf(React.PropTypes.shape({
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+    id: React.PropTypes.number.isRequired,
+  })).isRequired,
 };
 
 Application.defaultProps = {
-	title: "Scoreboard",
-};
-
-// React Component returning JSX
-function Application(props) {
-	return (
-		<div className="scoreboard">
-			<Header title={props.title} />
-
-			<div className="players">
-				{props.players.map(function(player) {
-					return <Player name={player.name} score={player.score} key={player.id}/>
-				})}
-			</div>
-		</div>
-	);
+  title: "Scoreboard",
 }
 
-
-
-// Rendering the React component to the Virtual DOM
-ReactDOM.render(<Application players={PLAYERS} />, document.getElementById('container'));
+ReactDOM.render(<Application players={PLAYERS}/>, document.getElementById('container'));
